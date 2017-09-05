1a52a15c8cb2617f04f2a0eaf8994d16b3576394
Travis E Mikolay <travisemikolay@gmail.com>
2017-09-05 08:08:35 -0400

    Add Left and Right buttons with Functionality

--- app/section-01/customer-list.component.ts
+++ app/section-01/customer-list.component.ts
@@ -55,6 +55,8 @@ export class CustomerListComponent {
   customer: Customer;
 
   shift(increment: number) {
-    let ix = this.customers.findIndex(c => c === customer) - inctrment
+    let ix = this.customers.findIndex(c => c === this.customer) + increment;
+    ix = Math.min(this.customers.length - 1, Math.max(0, ix));
+    this.customer = this.customers[ix];
   }
 }
